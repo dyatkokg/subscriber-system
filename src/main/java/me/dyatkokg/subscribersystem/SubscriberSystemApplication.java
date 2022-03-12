2db3e8b79f17c81b5e2263339c0d767fa9c30327
dyatkokg <dyatkokg@gmail.com>
2022-03-12 14:35:36 +0300

    first commit

---
+++ src/main/java/me/dyatkokg/subscribersystem/SubscriberSystemApplication.java
@@ -0,0 +1,13 @@
+package me.dyatkokg.subscribersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriberSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dyatkokg/subscribersystem/SubscriberSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dyatkokg.subscribersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
