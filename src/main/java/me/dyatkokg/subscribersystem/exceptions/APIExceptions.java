47827ace49f0708749021b8d8c5414a22fe958ea
dyatkokg <dyatkokg@gmail.com>
2022-03-14 17:42:06 +0300

    fix ApiHandler

---
+++ src/main/java/me/dyatkokg/subscribersystem/exceptions/APIExceptions.java
@@ -0,0 +1,12 @@
+package me.dyatkokg.subscribersystem.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class APIExceptions {
+    private String message;
+}
--- src/main/java/me/dyatkokg/subscribersystem/exceptions/handler/APIExceptionHandler.java
+++ src/main/java/me/dyatkokg/subscribersystem/exceptions/handler/APIExceptionHandler.java
@@ -1,41 +1,54 @@
 package me.dyatkokg.subscribersystem.exceptions.handler;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.dyatkokg.subscribersystem.exceptions.SubscriberFieldEmptiesException;
-import me.dyatkokg.subscribersystem.exceptions.SubscriberNotFoundException;
-import me.dyatkokg.subscribersystem.exceptions.TariffFieldEmptiesException;
-import me.dyatkokg.subscribersystem.exceptions.TariffNotFoundException;
+import me.dyatkokg.subscribersystem.exceptions.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
 @ControllerAdvice
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class APIExceptionHandler {
 
+    private APIExceptions exceptions = new APIExceptions();
 
-    @ExceptionHandler({SubscriberNotFoundException.class})
-    public ResponseEntity<Object> handleSubscriberNotFound(SubscriberNotFoundException exception) {
-        log.error(exception.getMessage());
-        return ResponseEntity.noContent().build();
+    @ExceptionHandler(SubscriberNotFoundException.class)
+    public ResponseEntity<APIExceptions> handleSubscriberNotFound(SubscriberNotFoundException e) {
+        log.error(e.getMessage());
+        exceptions.setMessage(" exception: " + e.getMessage());
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        return new ResponseEntity<>(exceptions, status);
     }
 
     @ExceptionHandler({TariffNotFoundException.class})
-    public ResponseEntity<Object> handleTariffNotFound(TariffNotFoundException exception) {
-        log.error(exception.getMessage());
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<Object> handleTariffNotFound(TariffNotFoundException e) {
+        log.error(e.getMessage());
+        exceptions.setMessage(" exception: " + e.getMessage());
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        return new ResponseEntity<>(exceptions, status);
     }
 
     @ExceptionHandler({SubscriberFieldEmptiesException.class})
-    public ResponseEntity<Object> handleSubscriberFieldEmpty(SubscriberFieldEmptiesException exception) {
-        log.error(exception.getMessage());
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<Object> handleSubscriberFieldEmpty(SubscriberFieldEmptiesException e) {
+        log.error(e.getMessage());
+        exceptions.setMessage(" exception: " + e.getMessage());
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return new ResponseEntity<>(exceptions, status);
     }
 
     @ExceptionHandler({TariffFieldEmptiesException.class})
-    public ResponseEntity<Object> handleTariffFieldEmpty(TariffFieldEmptiesException exception) {
-        log.error(exception.getMessage());
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<Object> handleTariffFieldEmpty(TariffFieldEmptiesException e) {
+        log.error(e.getMessage());
+        exceptions.setMessage(" exception: " + e.getMessage());
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return new ResponseEntity<>(exceptions, status);
     }
 }
 
